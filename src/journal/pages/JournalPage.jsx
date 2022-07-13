3c33948a9b683ee921170a3b54207f68c4b39c2a
edwin.martinez <EdwinSeb2014>
2022-07-12 22:23:30 -0500

    JournalPage - layout navbar, sidebar

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,11 +1,19 @@
 import { MailOutline } from "@mui/icons-material"
 import { Typography } from "@mui/material"
+import { JournalLayout } from "../layout/JournalLayout"
 
 export const JournalPage = () => {
+
   return (
-    <>
-      <Typography variant='h1' >JournalPage</Typography>
-      {/* <MailOutline /> */}
-    </>
+    <JournalLayout>
+      
+      <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, obcaecati! Commodi, sit rerum vel asperiores necessitatibus, incidunt est modi quia ut pariatur distinctio iure ea eius exercitationem, repellat illo eveniet!</Typography>
+
+      {/* NothingSelected */}
+
+      {/* NoteView */}
+
+    </JournalLayout>
+
   )
 }
